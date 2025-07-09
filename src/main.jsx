0a3b8d0b7ee7e0eb377028a4bf080e8e4c3d5f17
Pacho7895 <112191385+Pacho7895@users.noreply.github.com>
2025-07-09 15:41:49 -0500

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+import SolarKitPage from "./SolarKitPage";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SolarKitPage />
+  </React.StrictMode>
+);
